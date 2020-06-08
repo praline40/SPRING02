ae34f8f8e045aecd995d29cf6555f2e2ae8e36e8
Jules <praline40@hotmail.fr>
2020-06-08 15:26:52 -0400

    aa

---
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -0,0 +1,31 @@
+package com.wildcodeschool.doctor.controller;
+
+import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/{snumber}")
+    @ResponseBody
+    public Object doctor(@PathVariable String snumber) {
+        int number = Integer.parseInt(snumber);
+       
+        if (number >0 && number <13){
+            return "Erreur 303";
+        }
+
+        if (number == 13){
+            return new Doctor(number, "Jodie Whittaker"); 
+        }
+
+        return "Erreur 404 - Impossible de récupérer l'incarnation " + number;
+        
+            
+       
+        
+    }
+}
